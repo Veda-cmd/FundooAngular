eedbd4686df385acd3785399cf1e43b8dfa68f62
Vedant <vedant.nare04@gmail.com>
2020-02-05 19:18:11 +0530

    05/02/2020

--- src/app/components/dashboard/labelmenu/labelmenu.component.ts
+++ src/app/components/dashboard/labelmenu/labelmenu.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit, Input, ViewChild, Output, EventEmitter } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { MatCheckboxChange } from '@angular/material/checkbox';
-import { Event } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NoteService } from 'src/app/services/note.service';
 
 @Component({
   selector: 'app-labelmenu',
   templateUrl: './labelmenu.component.html',
   styleUrls: ['./labelmenu.component.scss']
 })
+
 export class LabelmenuComponent implements OnInit {
   labels:Array<any>;
   @Input() note:any;
   checked:boolean=false;
   @Output() changeLabels: EventEmitter<any> = new EventEmitter();
+  array:Array<any>=[];
+  value:string='';
+  list:boolean=true;
 
-
-  constructor(private data:DataService) { }
+  constructor(private data:DataService,private noteService:NoteService) { }
 
   ngOnInit() {
     this.data.labelArray.subscribe(value=>{
@@ -31,12 +35,44 @@ export class LabelmenuComponent implements OnInit {
     this.changeLabels.emit({value:event.checked,label:item})
   }
 
-  onClick(event:any){
-    event.stopPropagation();
+  addLabel(item:any){
+    console.log(item);
+    let request={
+      label_name:item
+    }
+    this.noteService.addLabel(request,null).subscribe((response:any)=>{
+      console.log(response.body);
+      this.changeLabels.emit({value:true,label:response.body});
+      this.data.changeLabels("labels");
+    },(error:HttpErrorResponse)=>{
+      console.log("Error");
+    })
+    this.value='';
+    this.array=[];
+    this.list=true;
   }
 
   searchLabel(event:any){
-    this.labels = this.labels.filter(element=>element.label_name == event.target.value);
+    // console.log(this.labels);
+    // this.value=event.target.value;
+    this.list=false;
+    let condition = new RegExp(event.target.value);
+    this.array = this.labels.filter(element=> {
+      return condition.test(element.label_name);
+    });
+
+    if(this.array.length===0){
+      this.value=event.target.value;
+    }
+    for(let i=0;i<this.array.length;i++){
+      if(this.array[i].label_name==event.target.value){
+        this.value='';
+      }
+      else{
+        this.value=event.target.value;
+      }
+    }
+    // console.log(this.array);
   }
 
 }
