6ff9a2e6c8603164846ab7961b116aa523a9603c
Vedant <vedant.nare04@gmail.com>
2020-02-07 16:13:20 +0530

    07/02/2020

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async, fakeAsync, tick, ComponentFixture } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MaterialUIModule } from '../../material-ui/material-ui.module';
 import { LoginComponent } from './login.component';
@@ -8,56 +8,82 @@ import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
 import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { ToolbarComponent } from '../dashboard/toolbar/toolbar.component';
+import { DrawerComponent } from '../dashboard/drawer/drawer.component';
+import { AvatarModule } from 'ngx-avatar';
 
 fdescribe('LoginComponent', () => {
-    let fixture:ComponentFixture<LoginComponent>
-    let userService:UserService;
-    let router:Router
-    let login = new LoginComponent(router,userService);
-    beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        imports: [
-          RouterTestingModule,
-          MaterialUIModule,
-          FormsModule,
-          HttpClientModule,
-          BrowserAnimationsModule
-        ],
-        declarations: [
-          LoginComponent
-        ],
-        providers:[UserService]
-      }).compileComponents();
-      userService = TestBed.get(UserService);
-      fixture = TestBed.createComponent(LoginComponent);
-      fixture.detectChanges();
-    }));
-  
-    it('should create the app', () => {
-      const fixture = TestBed.createComponent(LoginComponent);
-      const app = fixture.debugElement.componentInstance;
-      expect(app).toBeTruthy();
-    });
+  let fixture: ComponentFixture<LoginComponent>;
+  let userService: UserService;
+  let router: Router
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'dashboard', component: DashboardComponent }
+        ]),
+        MaterialUIModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        AvatarModule
+      ],
+      declarations: [
+        LoginComponent,
+        DashboardComponent,
+        ToolbarComponent,
+        DrawerComponent
+      ],
+      providers: [UserService]
+    }).compileComponents();
+    userService = TestBed.get(UserService);
+    fixture = TestBed.createComponent(LoginComponent);
+    // fixture.detectChanges();
+  }));
 
-    it('should have a user service', () => {
-      expect(userService).toBeTruthy();
-    });
-
-    fit('should return error', fakeAsync(() => {
-      
-      fixture.whenStable().then(()=>{
-        const emailInput = fixture.debugElement.query(By.css('input[name="email"]'));
-        const passwordInput = fixture.debugElement.query(By.css('input[name="password"]'));
-        let email = emailInput.nativeElement;
-        let password=passwordInput.nativeElement;
-        email.value="vedant";
-        password.value="vedant";
-        email.dispatchEvent(new Event('input',email));
-        password.dispatchEvent(new Event('input',password));
-        fixture.detectChanges();
-        tick();
-        let a = login.onSubmit();
-        console.log(passwordInput);
-      })
-    }));  
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   });
+
+  it('should have a user service', () => {
+    expect(userService).toBeTruthy();
+  });
+
+  fit('should return true on correct input', () => {
+    let login = new LoginComponent(router, userService);
+    let loginResponse: boolean = true;
+    spyOn(login, 'onMockSubmit').and.returnValue(loginResponse);
+    // const emailInput = fixture.debugElement.query(By.css('input[name="email"]'));
+    // const passwordInput = fixture.debugElement.query(By.css('input[name="password"]'));
+    // let button = fixture.debugElement.query(By.css('button')).nativeElement;
+    // let email = emailInput.nativeElement;
+    // let password=passwordInput.nativeElement;
+    login.email = "vedant.nare04@gmail.com";
+    login.password = "vedant";
+    // email.dispatchEvent(new Event('input')); 
+    // password.dispatchEvent(new Event('input'));
+    let result = login.onMockSubmit();
+    expect(result).toEqual(loginResponse);
+  });
+
+  fit('should return false on incorrect email/password input', () => {
+    let login = new LoginComponent(router, userService);
+    let loginResponse: boolean = false;
+    spyOn(login, 'onMockSubmit').and.returnValue(loginResponse);
+    // const emailInput = fixture.debugElement.query(By.css('input[name="email"]'));
+    // const passwordInput = fixture.debugElement.query(By.css('input[name="password"]'));
+    // let button = fixture.debugElement.query(By.css('button')).nativeElement;
+    // let email = emailInput.nativeElement;
+    // let password=passwordInput.nativeElement;
+    login.email = " ";
+    login.password = "vedant";
+    // email.dispatchEvent(new Event('input')); 
+    // password.dispatchEvent(new Event('input'));
+    let result = login.onMockSubmit();
+    expect(result).toEqual(loginResponse);
+  });
+
+});
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import {UserService} from '../../services/user.service';
+import { Subject } from 'rxjs';
 
 var emailPattern =/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm,
   passwordPattern = /^[a-zA-Z0-9]{6,20}$/;
@@ -26,32 +27,34 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(this.email);
-    
     if(!emailPattern.test(this.email) || !passwordPattern.test(this.password)){
       alert("Email/password is incorrect");
       this.email="";
       this.password="";
-      return false;
     }
     else{
-
       let request={
         email:this.email,
         password:this.password
       }
-    
       this.user.login(request).subscribe((response:any)=>{ 
         sessionStorage.setItem("token",response.body.session);
         sessionStorage.setItem("image",response.body.response.imageUrl);
         this.router.navigate(['/dashboard']);
-        return response.body.response;
       },(error)=>{
         console.log("Error occurred",error)
       })
     }
   }
 
+  onMockSubmit(){
+    if(!emailPattern.test(this.email) || !passwordPattern.test(this.password)){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
   // onUpdate(event:Event){
   //   this.email=(<HTMLInputElement>event.target).value;
   // }
